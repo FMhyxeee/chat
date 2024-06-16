c194ab3658b306b04ca2df01bdaf69f08e94f502
FMhyxeee <1179570770@qq.com>
2024-06-17 00:01:56 +0800

    features: support jwt signin & signup api

--- chat_server/src/config.rs
+++ chat_server/src/config.rs
@@ -1,21 +1,30 @@
 use std::{env, fs::File};
 
-use anyhow::bail;
+use anyhow::{bail, Result};
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct AppConfig {
     pub server: ServerConfig,
+    pub auth: AuthConfig,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct AuthConfig {
+    pub sk: String,
+    pub pk: String,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct ServerConfig {
     pub port: u16,
+    pub db_url: String,
 }
 
 impl AppConfig {
-    pub fn load() -> anyhow::Result<Self> {
+    pub fn load() -> Result<Self> {
         // read from  ./app.yml, or /etc/config/app.yml, or from env CHAT_CONFIG
+
         let ret = match (
             File::open("app.yml"),
             File::open("/etc/config/app.yml"),
--- chat_server/src/models/mod.rs
+++ chat_server/src/models/mod.rs
@@ -2,8 +2,11 @@ mod user;
 
 use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
+use sqlx::FromRow;
 
-#[derive(Debug, Clone, Serialize, Deserialize, sqlx::FromRow)]
+pub use user::{CreateUser, SigninUser};
+
+#[derive(Debug, Clone, FromRow, Serialize, Deserialize, PartialEq)]
 pub struct User {
     pub id: i64,
     pub fullname: String,
