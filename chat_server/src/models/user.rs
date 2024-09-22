df1d11e06a8083ea3650dddeb1f48677084f7d6d
FMhyxeee <1179570770@qq.com>
2024-09-22 21:23:38 +0800

    finish jwt signin signup

--- chat_server/src/models/user.rs
+++ chat_server/src/models/user.rs
@@ -1,6 +1,26 @@
+use std::mem;
+
+use argon2::{
+    password_hash::{rand_core::OsRng, SaltString},
+    Argon2, PasswordHash, PasswordHasher, PasswordVerifier,
+};
+use serde::{Deserialize, Serialize};
 use sqlx::PgPool;
 
 use crate::{AppError, User};
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct CreateUser {
+    pub fullname: String,
+    pub email: String,
+    pub password: String,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct SigninUser {
+    pub email: String,
+    pub password: String,
+}
 
 impl User {
     /// Find a user by email
@@ -11,5 +31,180 @@ impl User {
                 .fetch_optional(pool)
                 .await?;
         Ok(user)
+    }
+
+    // Create a new user
+    pub async fn create(input: &CreateUser, pool: &PgPool) -> Result<Self, AppError> {
+        let password_hash = hash_password(&input.password)?;
+        // check if email exists
+        let user = Self::find_by_email(&input.email, pool).await?;
+        if user.is_some() {
+            return Err(AppError::EmailAlreadyExists(input.email.clone()));
+        }
+
+        let user = sqlx::query_as(
+            r#"
+                INSERT INTO users (email, fullname, password_hash)
+                values ($1, $2, $3)
+                RETURNING id, fullname, email, created_at
+            "#,
+        )
+        .bind(&input.email)
+        .bind(&input.fullname)
+        .bind(password_hash)
+        .fetch_one(pool)
+        .await?;
+
+        Ok(user)
+    }
+
+    /// Verify email and password
+    pub async fn verify(input: &SigninUser, pool: &PgPool) -> Result<Option<Self>, AppError> {
+        let user: Option<User> = sqlx::query_as(
+            r#"
+                SELECT id, fullname, email, password_hash, created_at
+                FROM users
+                WHERE email = $1
+            "#,
+        )
+        .bind(&input.email)
+        .fetch_optional(pool)
+        .await?;
+
+        if let Some(mut user) = user {
+            let password_hash = mem::take(&mut user.password_hash);
+
+            let is_valid = verify_password(&input.password, &password_hash.unwrap_or_default())?;
+            if is_valid {
+                return Ok(Some(user));
+            } else {
+                return Ok(None);
+            }
+        }
+        Ok(None)
+    }
+}
+
+fn hash_password(password: &str) -> Result<String, AppError> {
+    let salt = SaltString::generate(&mut OsRng);
+
+    // Aragon2 with default params (Argon2id v19)
+    let argon2 = Argon2::default();
+
+    // Hash password to PHC string ($argon2id$v=19$...)
+    let password_hash = argon2
+        .hash_password(password.as_bytes(), &salt)?
+        .to_string();
+
+    Ok(password_hash)
+}
+
+fn verify_password(password: &str, password_hash: &str) -> Result<bool, AppError> {
+    let argon2 = Argon2::default();
+    let password_hash = PasswordHash::new(password_hash)?;
+
+    // Verify password
+    let is_valid = argon2
+        .verify_password(password.as_bytes(), &password_hash)
+        .is_ok();
+
+    Ok(is_valid)
+}
+
+#[cfg(test)]
+impl User {
+    pub fn new(id: i64, fullname: &str, email: &str) -> Self {
+        Self {
+            id,
+            fullname: fullname.to_string(),
+            email: email.to_string(),
+            password_hash: None,
+            created_at: chrono::Utc::now(),
+        }
+    }
+}
+
+#[cfg(test)]
+impl CreateUser {
+    pub fn new(fullname: &str, email: &str, password: &str) -> Self {
+        Self {
+            fullname: fullname.to_string(),
+            email: email.to_string(),
+            password: password.to_string(),
+        }
+    }
+}
+
+#[cfg(test)]
+impl SigninUser {
+    pub fn new(email: &str, password: &str) -> Self {
+        Self {
+            email: email.to_string(),
+            password: password.to_string(),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use anyhow::Result;
+    use sqlx_db_tester::TestPg;
+    use std::path::Path;
+
+    #[test]
+    fn hash_password_and_verify_should_work() -> Result<()> {
+        let password = "hunter42";
+        let password_hash = hash_password(password)?;
+        assert_eq!(password_hash.len(), 97);
+        assert!(verify_password(password, &password_hash)?);
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn create_duplicate_user_should_fail() -> Result<()> {
+        let tdb = TestPg::new(
+            "postgres://hyx:hyx@localhost:5432".to_string(),
+            Path::new("../migrations"),
+        );
+        let pool = tdb.get_pool().await;
+
+        let input = CreateUser::new("hyx", "hyx@me.org", "hunter42");
+        User::create(&input, &pool).await?;
+        let ret = User::create(&input, &pool).await;
+        match ret {
+            Err(AppError::EmailAlreadyExists(email)) => {
+                assert_eq!(email, input.email);
+            }
+            _ => panic!("Expecting EmailAlreadyExists error"),
+        }
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn create_and_verify_user_should_work() -> Result<()> {
+        let tdb = TestPg::new(
+            "postgres://hyx:hyx@localhost:5432".to_string(),
+            Path::new("../migrations"),
+        );
+        let pool = tdb.get_pool().await;
+
+        let input = CreateUser::new("hyx", "hyx@me.org", "hunter42");
+        let user = User::create(&input, &pool).await?;
+        assert_eq!(user.email, input.email);
+        assert_eq!(user.fullname, input.fullname);
+        assert!(user.id > 0);
+
+        let user = User::find_by_email(&input.email, &pool).await?;
+        assert!(user.is_some());
+        let user = user.unwrap();
+        assert_eq!(user.email, input.email);
+        assert_eq!(user.fullname, input.fullname);
+
+        let input = SigninUser::new(&input.email, &input.password);
+        let user = User::verify(&input, &pool).await?;
+        assert!(user.is_some());
+
+        Ok(())
     }
 }
--- migrations/20240917110208_init.sql
+++ migrations/20240917110208_init.sql
@@ -1,12 +1,12 @@
 -- this file is used for postgresql database initialization
 -- create user table
 CREATE TABLE IF NOT EXISTS users (
-    id SERIAL PRIMARY KEY,
+    id BIGSERIAL PRIMARY KEY,
     fullname VARCHAR(64) NOT NULL,
     email VARCHAR(64) NOT NULL,
-    -- hashed argon2 password
-    password VARCHAR(64) NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    -- hashed argon2 password, length 97
+    password_hash VARCHAR(97) NOT NULL,
+    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
 );
 
 -- create email index for users table
@@ -22,7 +22,7 @@ CREATE TABLE IF NOT EXISTS chats (
     type chat_type NOT NULL,
 
     members BIGINT[] NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
 );
 
 
@@ -33,10 +33,7 @@ CREATE TABLE IF NOT EXISTS messages (
     sender_id BIGINT NOT NULL,
     content TEXT NOT NULL,
     images TEXT[],
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-
-    FOREIGN KEY (chat_id) REFERENCES chats(id),
-    FOREIGN KEY (sender_id) REFERENCES users(id)
+    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
 );
 
 
